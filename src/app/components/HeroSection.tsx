adebe4c30d8025ec91c492c51b912a40e798b3f7
dylkim05 <dylanjkim123@gmail.com>
2025-07-19 12:59:17 -0500

    remove loading text

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -118,9 +118,7 @@ export default function HeroSection() {
           </motion.div>
         </>
       ) : (
-        <div className="flex h-full w-full items-center justify-center text-2xl font-bold">
-          Finding Dylan...
-        </div>
+        <div className="flex h-full w-full items-center justify-center text-2xl font-bold"></div>
       )}
     </section>
   );
