168cd145da71231c2af74a59a53a9dd115706b07
Dylan Kim <kim.dylan1@northeastern.edu>
2025-12-17 04:05:01 -0500

    remove toggle for experience/projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,60 +7,14 @@ import ExperienceSection from "./components/ExperienceSection";
 import ProjectSection from "./components/ProjectSection";
 
 export default function Home() {
-  const [showExperience, setShowExperience] = useState(true);
-
   return (
     <main
-      className={`container mx-auto flex flex-col items-center justify-center min-h-screen min-w-screen`}
+      className={`container mx-auto flex flex-col space-y-10 items-center justify-center min-h-screen min-w-screen bg-gradient-to-b from-background via-[rgb(240,240,220)] to-[rgb(220,220,200)] `}
     >
       <HeroSection />
       <SkillsSection />
-
-      <div className="flex justify-center w-screen bg-[rgb(220,220,200)] p-4">
-        <div className="rounded-lg p-1 shadow-md text-xl">
-          <button
-            onClick={() => setShowExperience(true)}
-            className={`px-6 py-3 rounded-md font-medium transition-all duration-200 ${
-              showExperience
-                ? "bg-[#E8E7D8] text-gray-800 shadow-sm"
-                : "text-gray-600 hover:text-gray-800"
-            }`}
-          >
-            Experience
-          </button>
-          <button
-            onClick={() => setShowExperience(false)}
-            className={`px-6 py-3 rounded-md font-medium transition-all duration-200 ${
-              !showExperience
-                ? "bg-[#E8E7D8] text-gray-800 shadow-sm"
-                : "text-gray-600 hover:text-gray-800"
-            }`}
-          >
-            Projects
-          </button>
-        </div>
-      </div>
-
-      <div className="relative">
-        <div
-          className={`transition-all duration-300 ease-in-out ${
-            showExperience
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 -translate-y-4 absolute top-0 left-0 w-full"
-          }`}
-        >
-          <ExperienceSection />
-        </div>
-        <div
-          className={`transition-all duration-300 ease-in-out ${
-            !showExperience
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 -translate-y-4 absolute top-0 left-0 w-full"
-          }`}
-        >
-          <ProjectSection />
-        </div>
-      </div>
+      <ExperienceSection />
+      <ProjectSection />
     </main>
   );
 }
